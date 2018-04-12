1ab4f86a28daef5c908007a482cb578657218c8f
Hanna S <hanna.soloman@gmail.com>
2018-04-12 12:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
