a1b1d7e77d7cfc111ffe014cadde2f9e147981c8
Hanna S <hanna.soloman@gmail.com>
2018-04-12 13:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key){
+function destructivelydeleteFromObjectByKey(object, key){
   delete object[key];
   return object
 }
