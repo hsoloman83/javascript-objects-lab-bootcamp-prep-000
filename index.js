c476b877de31b6edb4ddf1659fccd9078826889c
Hanna S <hanna.soloman@gmail.com>
2018-04-12 13:04:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
 
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, object, { [key]: value })
+}
+
 // var obj = { foo: 'bar' }
  
 // var newObj = Object.assign({}, obj)
