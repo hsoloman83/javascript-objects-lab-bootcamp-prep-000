7428901836b326b51e989ccc22f8d905cc107140
Hanna S <hanna.soloman@gmail.com>
2018-04-12 12:53:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ var recipes = {
   
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
+
+(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+
+  describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
+    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+      var obj = { prop: 1 }
+      
+      
