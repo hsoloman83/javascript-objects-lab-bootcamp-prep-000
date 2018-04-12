5ff220bbdd070e29077b0282c720b069bf72c543
Hanna S <hanna.soloman@gmail.com>
2018-04-12 12:42:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var recipes = {
 var object = {
   
 }
-// function updateObjectWithKeyAndValue(object, key, value){
-//   object[key] = value
-//   return object
-// }
+function updateObjectWithKeyAndValue(object, key, value){
+  Object.assign({}, { key: value })
+  return object
+}
 
-Object.assign({}, { key: value })
+
